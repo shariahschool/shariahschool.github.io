2251e8468d75d90ffbe843d3354ecdedf7e866b3
Shariah <119010414+shariahschool@users.noreply.github.com>
2024-03-07 19:07:50 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function fixTriangles(){
+    const list = document.getElementsByClassName('triangle');
+    console.log(list);
+    console.log(list.length);
+    for (let i = 0; i < list.length; i++) {
+        list[i].style.borderLeftWidth = document.body.clientWidth;;
+    }
+    const list2 = document.getElementsByClassName('invertedTriangle');
+    for (let i = 0; i < list2.length; i++) {
+        list2[i].style.borderRightWidth = document.body.clientWidth;;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', fixTriangles);
